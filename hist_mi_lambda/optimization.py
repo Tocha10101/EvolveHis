eb82c4612be8c6a3994ffaacbfea9223f03340f0
Tocha10101 <anton.masiukiewicz1@gmail.com>
2020-04-23 19:53:30 +0200

    Add files via upload

---
+++ hist_mi_lambda/optimization.py
@@ -0,0 +1,36 @@
+from numpy.random import uniform, normal
+from population import Population
+from individual import Individual
+
+class Optimization():
+
+    def __init__(self, init_params):
+        self.dim = init_params['dim']
+        self.lmbd = init_params['lmbd']
+        self.mu = init_params['mu']
+        self.initial_len = init_params['initial_len']
+        
+        initial = {
+            'dim': self.dim,
+            'individuals': self.generate_population(),
+            'lambda': self.lmbd,
+            'mu': self.mu
+        }
+        self.population = Population(initial)
+
+    def generate_population(self):
+        individs = []
+        for i in range(self.initial_len):
+            arguments, sigmas = [], []
+            for j in range(self.dim):
+                # give it some use!!!
+                argument = uniform(-100, 100)
+                sigma = uniform(0, 25) 
+                arguments.append(argument)
+                sigmas.append(sigma)
+            individs.append( Individual({
+                'arguments': arguments,
+                'sigmas': sigmas
+                })
+            )
+        return individs
