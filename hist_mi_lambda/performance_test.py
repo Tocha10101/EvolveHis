e3b381c6f386ea6d6ca0bb62c6cae64d6ce181cc
Tocha10101 <anton.masiukiewicz1@gmail.com>
2020-05-01 21:00:13 +0200

    Update performance_test.py

--- hist_mi_lambda/performance_test.py
+++ hist_mi_lambda/performance_test.py
@@ -6,7 +6,7 @@ optimization = None
 input_filename = "calls_metadata_2d.csv"
 output_filename = "result_data2d.csv"
 def test_drive(input_file, output_file):
-    with open('calls_metadata_2d.csv', 'r') as csv_read, open('result_data.csv', 'w', newline='') as csv_write:
+    with open(input_filename, 'r') as csv_read, open(output_filename, 'w', newline='') as csv_write:
         csv_reader = DictReader(csv_read, delimiter=',')
         fieldnames = [item for item in csv_reader.fieldnames]
         fieldnames.remove('Function num')
